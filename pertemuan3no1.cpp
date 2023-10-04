42007b0fa0cbb9203fa56f4e81f66c7ac7594741
zaidanann <146158827+zaidanann@users.noreply.github.com>
2023-10-04 17:08:31 +0700

    Add files via upload

---
+++ pertemuan3no1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int bilangan;
+  cout<<"masukan angka : ";
+  cin>>bilangan;
+  cout<<endl;
+
+  if (bilangan%2==0){
+    cout<<"bilangan genap"<<endl;
+  }else {
+    cout<<" bilangan ganjil"<<endl;
+  }
+}
